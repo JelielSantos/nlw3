efab4bef6d6bbe821bddb6b512df396d8c3b22cc
JelielSantos <jelielsantos00@gmail.com>
2020-11-09 21:12:01 -0300

    to com fome

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,11 +1,34 @@
-import { Response, Request} from 'express';
+import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import multer from 'multer';
 
 import Orphanage from '../models/Orphanage';
 
 
+
+
 export default {
-    async create(response: Response, request: Request) {
+    async index(request: Request, response: Response){
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.find({
+            relations:['images']
+        });
+
+        return response.json(orphanages);
+    },
+
+    async show(request: Request, response: Response){
+        const { id } = request.params;
+        const orphanagesRepository = getRepository(Orphanage);
+ 
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+        return response.json(orphanage);
+    },
+
+    async create(request: Request, response: Response) {
         const {
             name,
             latitude,
@@ -14,9 +37,16 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+            
         } = request.body;
 
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+        
+        const images = requestImages.map(image => {
+            return { path: image.filename}
+        })
 
         const orphanages = orphanagesRepository.create({
             name,
@@ -26,6 +56,8 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+            
+            
         });
 
         await orphanagesRepository.save(orphanages);
